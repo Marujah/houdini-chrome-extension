97762c27640fb3255ec870d4df0993980369027f
Mhiri, Marouen <Marouen.Mhiri@EXXETA.com>
2019-10-02 15:31:42 +0200

    Hou-1: add api items and update styles

--- devtools.js
+++ devtools.js
@@ -4,5 +4,19 @@ chrome.devtools.panels.create(
     "panel.html",
     function(panel) {
         console.log('inside the panel', panel);
+        chrome.extension.onRequest.addListener(
+            function (request, sender, sendResponse) {
+                // LOG THE CONTENTS HERE
+                console.log(request.content);
+            });
+
+        chrome.tabs.getSelected(null, function (tab) {
+
+            // Now inject a script onto the page
+            chrome.tabs.executeScript(tab.id, {
+                code: "chrome.extension.sendRequest({content: document.scripts}, function(response) { console.log('success'); });"
+            }, function () { console.log('done'); });
+
+        });
     }
 );
